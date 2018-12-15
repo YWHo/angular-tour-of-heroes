d9ca73b17e11805456f9f2e03f174cd8954f430c
Brian Ho <YWHo@users.noreply.github.com>
2018-12-16 10:53:17 +1300

    add http get request

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', function(req, res) {
-    res.json({text:'Hello World'})
+router.get('/', (req, res) => {
+  res.json({text:'Hello World'})
+})
+
+router.get('/heroes', (req, res) => {
+  const heroes = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
+  res.json(heroes)
 })
 
 module.exports = router
