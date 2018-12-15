906f2203f4f1338553338e605d812fe4847827ac
Brian Ho <YWHo@users.noreply.github.com>
2018-12-15 19:43:39 +1300

    add nodemon for development

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const express = require('express')
 const server = express()
+const routes = require('./routes')
 
 server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, '../dist/angular-tour-of-heroes')))
+server.use('/api', routes)
+
 
 module.exports = server
